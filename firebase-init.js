f066ce63d5ceb11554a9b23151f8a3a59c20b1bf
eloysepaes <eloysepaess@gmail.com>
2025-12-11 11:31:27 -0300

    Add files via upload

---
+++ firebase-init.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+     apiKey: "AIzaSyDHifYpUji-2TU7qrkFGbAjEEkzSbiZl4Q",
+  authDomain: "projeto-login-prototipo-85b09.firebaseapp.com",
+  projectId: "projeto-login-prototipo-85b09",
+  storageBucket: "projeto-login-prototipo-85b09.firebasestorage.app",
+  messagingSenderId: "431450578437",
+  appId: "1:431450578437:web:ddadd2674c3e62f625866c",
+  measurementId: "G-F4W13SLEMB"
+  };
+
+firebase.initializeApp(firebaseConfig); 
+  
